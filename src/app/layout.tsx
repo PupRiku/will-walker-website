7bf258bc24c61286a357211d6b94e8c120c46ca0
Chris Diorio <chris.diorio12@gmail.com>
2025-06-16 23:01:27 -0500

    Improve some SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,9 @@ const specialElite = Special_Elite({
 });
 
 export const metadata: Metadata = {
-  title: 'Will Walker, Playwright',
-  description: 'The official website for playwright Will Walker.',
+  title: 'William L. Walker Montgomerie | Playwright, Director, Educator',
+  description:
+    'Explore the selected works and portfolio of William L. Walker Montgomerie, a playwright and director based in Paris, Texas. Read samples and get in contact.',
 };
 
 export default function RootLayout({
@@ -40,6 +41,25 @@ export default function RootLayout({
       lang="en"
       className={`${lora.variable} ${lato.variable} ${specialElite.variable}`}
     >
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'Person',
+              name: 'William L. Walker Montgomerie',
+              jobTitle: 'Playwright, Director, Educator',
+              url: 'https://will-walker-website.vercel.app/',
+              sameAs: [
+                // Add links to his social media or professional profiles here
+                // "https://www.linkedin.com/in/williamwalker",
+                // "https://twitter.com/williamwalker"
+              ],
+            }),
+          }}
+        />
+      </head>
       <body>
         <Header />
         <main className="main-content">{children}</main>
