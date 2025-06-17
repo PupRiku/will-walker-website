66343a479746934e2d4ec8496214d6070ac301e9
Chris Diorio <chris.diorio12@gmail.com>
2025-06-16 21:34:44 -0500

    Solve 403 error

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -2,5 +2,5 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const { origin } = new URL(request.url);
-  return NextResponse.redirect(`${origin}/thank-you`);
+  return NextResponse.redirect(`${origin}/thank-you`, { status: 303 });
 }
