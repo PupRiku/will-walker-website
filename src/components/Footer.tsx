5817d502f76de2207dc35f3448a97aff4bb51b51
Chris Diorio <chris.diorio12@gmail.com>
2025-08-03 18:53:45 -0500

    Add social links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,22 @@
 import Link from 'next/link';
+import SocialLinks from './SocialLinks';
 import styles from './Footer.module.css';
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
-      <p className={styles.text}>
-        &copy; {currentYear} Will Walker. All Rights Reserved.
-      </p>
-      <Link href="/cv" className={styles.footerLink}>
-        CV
-      </Link>
+      <div className={styles.footerInner}>
+        <div className={styles.footerLinks}>
+          <p className={styles.text}>
+            &copy; {currentYear} William L. Walker. All Rights Reserved.
+          </p>
+          <Link href="/cv" className={styles.footerLink}>
+            CV
+          </Link>
+        </div>
+        <SocialLinks />
+      </div>
     </footer>
   );
 }
