9141456905bcd2bc5b5c6ee695bb3488af93f043
Chris Diorio <chris.diorio12@gmail.com>
2025-08-03 18:12:10 -0500

    Add CV link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './Footer.module.css';
 
 export default function Footer() {
@@ -7,6 +8,9 @@ export default function Footer() {
       <p className={styles.text}>
         &copy; {currentYear} Will Walker. All Rights Reserved.
       </p>
+      <Link href="/cv" className={styles.footerLink}>
+        CV
+      </Link>
     </footer>
   );
 }
